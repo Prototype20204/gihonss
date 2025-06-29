6a0ac2a54798a345e3042f717b0a22ab1f26de79
Mach Deng Mach <freedom.hotel09@gmail.com>
2025-06-29 03:45:16 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,7 @@ const App: React.FC = () => {
           style={{ opacity: 0 }}  // This ensures it's initially invisible but still rendered
         >
           <img
-            src="./public/assets/images/GIHON CYBER"
+            src="./public/assets/images/GIHON CYBER SECURITY.jpg"
             alt="hacker"
           />
         </div>
@@ -295,12 +295,12 @@ const App: React.FC = () => {
             <div className="grid grid-cols-2 gap-4 mt-8">
             <img
               className="w-full transition-all duration-300 hover:saturate-150 hover:brightness-75 hover:hue-rotate-15"
-              src="./public/assets/images/GIHON CYBER"
+              src="./public/assets/images/GIHON CYBER SECURITY.jpg"
               alt="office content 1"
             />
             <img
               className="mt-4 w-full lg:mt-10 transition-all duration-300 hover:saturate-150 hover:brightness-75 hover:hue-rotate-15"
-              src="./public/assets/images/GIHON CYBER
+              src="./public/assets/images/GIHON CYBER SECURITY.jpg
               alt="office content 2"
             />
           </div>
@@ -326,7 +326,7 @@ const App: React.FC = () => {
               <figcaption className="flex items-center justify-center mt-6 space-x-3">
               <img
                 className="w-6 h-6 rounded-full transition-opacity duration-300 hover:opacity-70"
-                src="./public/assets/images/GIHON CYBER"
+                src="./public/assets/images/GIHON CYBER SECURITY.jpg"
                 alt="profile picture"
               />
                 <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
@@ -345,7 +345,7 @@ const App: React.FC = () => {
           <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
             <img
               className="w-full transition-opacity duration-300 hover:opacity-70"
-              src="./public/assets/images/GIHON CYBER"
+              src="./public/assets/images/GIHON CYBER SECURITY.jpg"
               alt="dashboard image"
             />
             <div className="mt-4 md:mt-0">
